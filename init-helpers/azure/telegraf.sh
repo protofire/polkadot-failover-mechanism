8677481a5488eb94d1d213847b15ec58e0a05e66
Arsenii Petrovich <arseniy.altoros@gmail.com>
2020-05-26 01:28:45 +0300

    Add telegraf init helper

---
+++ init-helpers/azure/telegraf.sh
@@ -0,0 +1,83 @@
+cat < /etc/telegraf/telegraf.conf
+[global_tags]
+prefix = "$1" # will tag all metrics with dc=us-east-1
+
+# Configuration for telegraf agent
+[agent]
+  interval = "60s"
+  round_interval = true
+  hostname = "$2"
+  omit_hostname = false
+
+
+###############################################################################
+#                            OUTPUT PLUGINS                                   #
+###############################################################################
+
+
+# # Send aggregate metrics to Azure Monitor
+[[outputs.azure_monitor]]
+timeout = "20s"
+namespace_prefix = "$1/"
+
+###############################################################################
+#                            INPUT PLUGINS                                    #
+###############################################################################
+
+
+# Read metrics about cpu usage
+[[inputs.cpu]]
+  totalcpu = true
+
+datacenter = "$1"
+cat < /etc/telegraf/telegraf.conf
+# Read metrics about disk usage by mount point
+[[inputs.disk]]
+  mount_points = ["/data", "/"]
+  ignore_fs = ["tmpfs", "devtmpfs", "devfs", "iso9660", "overlay", "aufs", "squashfs"]
+
+# Read metrics about memory usage
+[[inputs.mem]]
+  # no configuration
+
+# Read metrics about swap memory usage
+[[inputs.swap]]
+  # no configuration
+
+[[inputs.consul]]
+datacenter = "$1"
+
+[[inputs.http_listener_v2]]
+  service_address = ":12500"
+  path = "/telegraf"
+  max_body_size = "1MB"
+  data_format = "influx"
+ubuntu@selfportal:~$ vim telegraf.conf
+ubuntu@selfportal:~$ cat telegraf.conf 
+cat < /etc/telegraf/telegraf.conf
+[global_tags]
+prefix = "$1" # will tag all metrics with dc=us-east-1
+
+
+# Read metrics about disk usage by mount point
+[[inputs.disk]]
+  mount_points = ["/data", "/"]
+  ignore_fs = ["tmpfs", "devtmpfs", "devfs", "iso9660", "overlay", "aufs", "squashfs"]
+
+# Read metrics about memory usage
+[[inputs.mem]]
+  # no configuration
+
+# Read metrics about swap memory usage
+[[inputs.swap]]
+  # no configuration
+
+[[inputs.consul]]
+datacenter = "$1"
+
+[[inputs.http_listener_v2]]
+  service_address = ":12500"
+  path = "/telegraf"
+  max_body_size = "1MB"
+  data_format = "influx"
+EOF
