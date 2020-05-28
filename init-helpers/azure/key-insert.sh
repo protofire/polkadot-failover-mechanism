ff0ce1e9ec36c3d66628cba4911122daa2b4e897
Arsenii Petrovich <arseniy.altoros@gmail.com>
2020-05-28 21:17:23 +0300

    Update helpers

---
+++ init-helpers/azure/key-insert.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -x -e -E
+
+key_names=$(az keyvault secret list --vault-name ${1}-vault | jq .[].name -r | grep '\-keys\-' | cut -d '-' -f4 | uniq)
+
+for key_name in ${key_names[@]} ; do
+
+  echo "Adding key $key_name"
+  SEED=$(az keyvault secret show --vault-name ${prefix}-vault --name polkadot-${1}-keys-${key_name}-seed | jq .value -r)
+  KEY=$(az keyvault secret show --vault-name ${prefix}-vault --name polkadot-${1}-keys-${key_name}-key | jq .value -r)
+  TYPE=$(az keyvault secret show --vault-name ${prefix}-vault --name polkadot-${1}-keys-${key_name}-type| jq .value -r)
+   
+ 
+    curl -s -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "author_insertKey", "params":["'"$TYPE"'","'"$SEED"'","'"$KEY"'"]}' http://localhost:9933
+
+done
