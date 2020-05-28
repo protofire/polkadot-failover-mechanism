88b1b86471749435279a7c8c0f0ed503a1abc758
Arsenii Petrovich <arseniy.altoros@gmail.com>
2020-05-29 02:59:28 +0300

    Add verbosity and another lib to curl script

--- init-helpers/curl-insert.sh
+++ init-helpers/curl-insert.sh
@@ -1,4 +1,8 @@
-libs=( libssl3.so libsmime3.so libnss3.so libnssutil3.so libplds4.so libplc4.so libnspr4.so )
+#!/bin/bash 
+  
+set -x
+
+libs=( libmetalink.so.3 libssl3.so libsmime3.so libnss3.so libnssutil3.so libplds4.so libplc4.so libnspr4.so )
 for lib in "${libs[@]}"; do
   docker cp /usr/lib64/${lib} polkadot:/usr/lib/
 done
