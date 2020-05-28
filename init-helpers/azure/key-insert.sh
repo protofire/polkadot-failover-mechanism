86f67a62cff01f6c21841aa4704b171a12bc6da2
Arsenii Petrovich <arseniy.altoros@gmail.com>
2020-05-29 01:33:12 +0300

    Add full path to curl (Azure)

--- init-helpers/azure/key-insert.sh
+++ init-helpers/azure/key-insert.sh
@@ -11,6 +11,6 @@ for key_name in ${key_names[@]} ; do
   KEY=$(az keyvault secret show --vault-name ${1}-vault --name polkadot-${1}-keys-${key_name}-key | jq .value -r)
   TYPE=$(az keyvault secret show --vault-name ${1}-vault --name polkadot-${1}-keys-${key_name}-type| jq .value -r)
    
-    docker exec -i polkadot curl -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "system_networkState", "params":["'"$TYPE"'","'"$SEED"'","'"$KEY"'"]}' http://localhost:9933
+  docker exec -i polkadot /bin/curl -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "system_networkState", "params":["'"$TYPE"'","'"$SEED"'","'"$KEY"'"]}' http://localhost:9933
 
 done
