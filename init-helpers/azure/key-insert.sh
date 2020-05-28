8f5b4355493e0e616925654056f3e73f9dc16dc0
Arsenii Petrovich <arseniy.altoros@gmail.com>
2020-05-28 22:41:49 +0300

    Update key-insert.sh

--- init-helpers/azure/key-insert.sh
+++ init-helpers/azure/key-insert.sh
@@ -7,9 +7,9 @@ key_names=$(az keyvault secret list --vault-name ${1}-vault | jq .[].name -r | g
 for key_name in ${key_names[@]} ; do
 
   echo "Adding key $key_name"
-  SEED=$(az keyvault secret show --vault-name ${prefix}-vault --name polkadot-${1}-keys-${key_name}-seed | jq .value -r)
-  KEY=$(az keyvault secret show --vault-name ${prefix}-vault --name polkadot-${1}-keys-${key_name}-key | jq .value -r)
-  TYPE=$(az keyvault secret show --vault-name ${prefix}-vault --name polkadot-${1}-keys-${key_name}-type| jq .value -r)
+  SEED=$(az keyvault secret show --vault-name ${1}-vault --name polkadot-${1}-keys-${key_name}-seed | jq .value -r)
+  KEY=$(az keyvault secret show --vault-name ${1}-vault --name polkadot-${1}-keys-${key_name}-key | jq .value -r)
+  TYPE=$(az keyvault secret show --vault-name ${1}-vault --name polkadot-${1}-keys-${key_name}-type| jq .value -r)
    
  
     curl -s -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "author_insertKey", "params":["'"$TYPE"'","'"$SEED"'","'"$KEY"'"]}' http://localhost:9933
