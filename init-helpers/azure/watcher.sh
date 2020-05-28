f0bc3564953c49495b748c0d3134efc7e8f9febb
Arsenii Petrovich <arseniy.altoros@gmail.com>
2020-05-29 01:42:15 +0300

    Update watcher.sh

--- init-helpers/azure/watcher.sh
+++ init-helpers/azure/watcher.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-$(docker inspect -f "{{.State.Running}}" polkadot && curl -s -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "system_health", "params":[]}' http://127.0.0.1:9933);
+$(docker inspect -f "{{.State.Running}}" polkadot && docker exec -i polkadot /bin/curl -s -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "system_health", "params":[]}' http://127.0.0.1:9933);
 STATE=$?
-BLOCK_NUMBER=$(($(curl -s -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "chain_getBlock", "params":[]}' http://127.0.0.1:9933 | jq .result.block.header.number -r)))
-AMIVALIDATOR=$(curl -s -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "system_nodeRoles", "params":[]}' http://127.0.0.1:9933 | jq -r .result[0])
+BLOCK_NUMBER=$(($(docker exec -i polkadot /bin/curl -s -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "chain_getBlock", "params":[]}' http://127.0.0.1:9933 | jq .result.block.header.number -r)))
+AMIVALIDATOR=$(docker exec -i polkadot /bin/curl -s -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "system_nodeRoles", "params":[]}' http://127.0.0.1:9933 | jq -r .result[0])
 if [ "$AMIVALIDATOR" == "Authority" ]; then
   AMIVALIDATOR=1
 else
