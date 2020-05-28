08a1fa95999a6138800be5ad3c4aeee4f47c5491
Arsenii Petrovich <arseniy.altoros@gmail.com>
2020-05-29 02:03:04 +0300

    Create new helper script to install curl to docker container

---
+++ init-helpers/curl-insert.sh
@@ -0,0 +1,6 @@
+libs=( libssl3.so libsmime3.so libnss3.so libnssutil3.so libplds4.so libplc4.so libnspr4.so )
+for lib in "${libs[@]}"; do
+  docker cp /usr/lib64/${lib} polkadot:/usr/lib/
+done
+
+docker cp /usr/bin/curl polkadot:/bin/curl
